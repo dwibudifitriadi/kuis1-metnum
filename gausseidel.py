9beed03111eb4a5790eb8972939c1c2ba25a011f
Dwi Budi Fitri Adi <116490361+dwibudifitriadi@users.noreply.github.com>
2025-04-22 12:23:41 +0700

    ganti jadi list biasa

--- gausseidel.py
+++ gausseidel.py
@@ -1,30 +1,23 @@
-import numpy as np
-
-matrix = np.array([[4,1,1,7],
-             [-2,6,1,9],
-             [1,1,5,-6]], dtype=float)
-
 def gauss_seidel(A, b, x0, max_iter, tol):
     n = len(A)
     x = x0[:]
     for itr in range(max_iter):
         x_old = x[:]
         for i in range(n):
-            sigma = sum(A[i][j] * x[j] for j in range(n) if j != i)
-            x[i] = (b[i] - sigma) / A[i][i]
+           sigma = sum(A[i][j] * x[j] for j in range(n) if j != i)
+           x[i] = (b[i] - sigma) / A[i][i]
         if max(abs(x[i] - x_old[i]) for i in range(n)) < tol:
             return x
     return x
-
 max_iter = 100
+A = [[4, -1, 1],
+    [-2, 6, 1],
+    [1, 1, 5]]
+b = [7, 9, -6]
+x0 = [0,0,0]
 tol = 1e-6
-A = matrix[:,:-1]
-b = matrix[:,-1]
-x0 = np.zeros(len(b))
-
 solusi = gauss_seidel(A,b,x0,max_iter,tol)
-print(solusi)
-
+print(f"x0 = {solusi[0]:.5f}, x1 = {solusi[1]:.5f}, x2 = {solusi[2]:.5f}")
 # Nama : Dwi Budi Fitri Adi
 # Kelas : TIF RP 24D
 # NPM : 24552011093
